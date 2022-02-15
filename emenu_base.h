0adeae7d5a4f61f2f708a28c895cbb1968276be7
Pedro Larroy <pedro.larroy@gmail.com>
2022-02-15 00:05:42 -0800

    emenu base

---
+++ emenu_base.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace emenu {
+
+typedef int16_t dim_t;
+typedef int16_t color_t;
+
+
+class Screen;
+class Menu;
+class MenuItem;
+class Label;
+class DValue;
+template<typename T>
+class IValueInput;
+
+}
